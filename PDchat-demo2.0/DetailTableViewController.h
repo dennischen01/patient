70a7bea795a9eb3bf683f6d1113667e6c2b7d534
dennischen01 <1103015686@qq.com>
2017-05-07 15:18:43 +0800

    修改控制器

---
+++ PDchat-demo2.0/DetailTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailTableViewController.h
+//  patient
+//
+//  Created by 陈希灿 on 2017/5/7.
+//  Copyright © 2017年 hdu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailTableViewController : UITableViewController
+
+@end
